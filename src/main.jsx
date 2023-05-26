a30e81864533ec1943794d9f166a715dc21225f9
Gregory <gregory_berezin@outlook.com>
2023-05-26 23:07:15 +0300

    new edits

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,12 @@ import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+
     <Provider store={store}>
+      
       <App />
+
     </Provider>
+
   </React.StrictMode>
 );
